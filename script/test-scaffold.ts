f28ec917d6a5df1494df16dbe693c8b6294ddafa
Wim Tibackx <tibackx.wim@gmail.com>
2024-05-25 15:25:14 +0200

    test: Revert to initial test-scaffold script to avoid making unnecessary changes

--- script/test-scaffold.ts
+++ script/test-scaffold.ts
@@ -1,42 +1,24 @@
 import path from 'path';
 import fs from 'fs';
 import { execSync } from 'child_process';
-import { NPM_RC_CONTENTS, NPM_RC_FILE, PNPM_STORE_PATH } from './test-utils';
 
-if (!fs.existsSync(PNPM_STORE_PATH)) {
-    fs.mkdirSync(PNPM_STORE_PATH)
+const scaffoldPath = path.join(__dirname, '../.test/scaffold');
+if (fs.existsSync(scaffoldPath)) {
+    fs.rmSync(scaffoldPath, { recursive: true, force: true });
 }
+fs.mkdirSync(scaffoldPath, { recursive: true });
 
-function run(cmd: string, path: string) {
-    console.log(`Running: ${cmd}, in ${path}`);
+function run(cmd: string) {
+    console.log(`Running: ${cmd}, in ${scaffoldPath}`);
     try {
-        execSync(cmd, { cwd: path, stdio: 'ignore' });
+        execSync(cmd, { cwd: scaffoldPath, stdio: 'ignore' });
     } catch (err) {
         console.error(`Test project scaffolding cmd error: ${err}`);
         throw err;
     }
 }
 
-function setUpProject(projectPath: string) {
-    if (fs.existsSync(projectPath)) {
-        const dirEntries = fs.readdirSync(projectPath, {withFileTypes: true});
-        for (const entry of dirEntries) {
-            const keep = entry.isFile() && ['package.json', '.gitignore'].includes(entry.name);
-            if (!keep) {
-                fs.rmSync(path.join(entry.path, entry.name), { force: true, recursive: true });
-                console.log('removed ' + path.join(entry.path, entry.name));
-            }
-        }
-    } else {
-        fs.mkdirSync(projectPath, { recursive: true });
-        run('npm init -y', projectPath);
-    }
-    fs.writeFileSync(path.join(projectPath, NPM_RC_FILE), NPM_RC_CONTENTS, { encoding: 'utf-8', flag: 'w+' });
-    run('pnpm i --ignore-workspace', projectPath);
-}
-
-const scaffoldPath = path.join(__dirname, '../.test/scaffold');
-setUpProject(scaffoldPath);
-run('pnpm i --ignore-workspace typescript prisma @prisma/client zod decimal.js @types/node', scaffoldPath);
+run('npm init -y');
+run('npm i --no-audit --no-fund typescript prisma @prisma/client zod decimal.js @types/node');
 
 console.log('Test scaffold setup complete.');
